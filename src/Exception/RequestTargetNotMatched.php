9ea6de1fedfacbe151622e069f1aece5032ba194
Nathan Bishop <nbish11@hotmail.com>
2019-07-18 10:41:31 +1000

    Add exception detailing when a route failed because of the request-target

---
+++ src/Exception/RequestTargetNotMatched.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Route\Exception;
+
+use RuntimeException;
+use Meraki\Route\Exception as RouteException;
+
+/**
+ * Exception used when the pattern of a route could not be matched against the request-target.
+ *
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2019 Nathan Bishop
+ * @license The MIT license.
+ */
+final class RequestTargetNotMatched extends RuntimeException implements RouteException
+{
+    /**
+     *
+     */
+    public const EQUIVALENT_STATUS_CODE = 404;
+
+    /**
+     * @var string [$failedRequestTarget description]
+     */
+    private $failedRequestTarget;
+
+    /**
+     * [__construct description]
+     *
+     * @param string $failedRequestTarget [description]
+     */
+    public function __construct(string $failedRequestTarget)
+    {
+        $this->failedRequestTarget = $failedRequestTarget;
+
+        parent::__construct($this->generateMessage(), self::EQUIVALENT_STATUS_CODE);
+    }
+
+    /**
+     * [getFailedRequestTarget description]
+     *
+     * @return string [description]
+     */
+    public function getFailedRequestTarget(): string
+    {
+        return $this->failedRequestTarget;
+    }
+
+    /**
+     * [generateMessage description]
+     *
+     * @return string [description]
+     */
+    private function generateMessage(): string
+    {
+    	return sprintf('The request target "%s" could not be matched!', $this->failedRequestTarget);
+    }
+}
---
+++ tests/Exception/RequestTargetNotMatchedTest.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Route\Exception;
+
+use Meraki\TestSuite;
+use Meraki\Route\Exception\RequestTargetNotMatched;
+use Meraki\Route\Exception as RouteException;
+use RuntimeException;
+
+final class RequestTargetNotMatchedTest extends TestSuite
+{
+    /**
+     * @test
+     */
+    public function it_implements_the_route_components_base_exception(): void
+    {
+        $requestTargetNotMatched = new RequestTargetNotMatched('*');
+
+        $this->assertInstanceOf(RouteException::class, $requestTargetNotMatched);
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_a_runtime_exception(): void
+    {
+        $requestTargetNotMatched = new RequestTargetNotMatched('*');
+
+        $this->assertInstanceOf(RuntimeException::class, $requestTargetNotMatched);
+    }
+
+    /**
+     * @test
+     */
+    public function sets_correct_message(): void
+    {
+        $expectedMessage = 'The request target "*" could not be matched!';
+        $requestTargetNotMatched = new RequestTargetNotMatched('*');
+
+        $actualMessage = $requestTargetNotMatched->getMessage();
+
+        $this->assertEquals($expectedMessage, $actualMessage);
+    }
+
+    /**
+     * @test
+     */
+    public function can_get_failed_request_target(): void
+    {
+        $expectedRequestTarget = '*';
+        $requestTargetNotMatched = new RequestTargetNotMatched($expectedRequestTarget);
+
+        $actualRequestTarget = $requestTargetNotMatched->getFailedRequestTarget();
+
+        $this->assertEquals($expectedRequestTarget, $actualRequestTarget);
+    }
+
+    /**
+     * @test
+     */
+    public function code_is_set_to_the_equivalent_http_status_code(): void
+    {
+        $expectedCode = 404;
+        $requestTargetNotMatched = new RequestTargetNotMatched('*');
+
+        $actualCode = $requestTargetNotMatched->getCode();
+
+        $this->assertEquals($expectedCode, $actualCode);
+    }
+}
