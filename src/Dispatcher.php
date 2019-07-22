11fed71cd1ade208b2791f760a38f60ffb72a46f
Nathan Bishop <nbish11@hotmail.com>
2019-07-22 14:18:05 +1000

    Add support for dispatching matched routes

---
+++ src/Dispatcher.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Route;
+
+use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Meraki\Route\Matcher;
+use Psr\Http\Message\ServerRequestInterface as ServerRequest;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Meraki\Route\MatchResult;
+use Meraki\Route\Exception\MethodNotMatched as MethodNotMatchedException;
+use Meraki\Route\Exception\RequestTargetNotMatched as RequestTargetNotMatchedException;
+
+/**
+ * Invokes the matched request-handler or modifies the response in case of failure.
+ *
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2019 Nathan Bishop
+ * @license The MIT license.
+ */
+final class Dispatcher implements Middleware
+{
+	/**
+	 * @var Matcher [$matcher description]
+	 */
+	private $matcher;
+
+	/**
+	 * [__construct description]
+	 *
+	 * @param Matcher $matcher [description]
+	 */
+	public function __construct(Matcher $matcher)
+	{
+		$this->matcher = $matcher;
+	}
+
+    /**
+     * [process description]
+     *
+     * @param ServerRequest  $request [description]
+     * @param RequestHandler $handler [description]
+     * @return Response [description]
+     */
+    public function process(ServerRequest $request, RequestHandler $handler): Response
+    {
+    	$result = $this->matcher->match($request);
+
+    	if ($result->isSuccessful()) {
+    		$rule = $result->getMatchedRule();
+
+			foreach ($rule->getPattern()->getPlaceholders() as $key => $value) {
+				$request = $request->withAttribute($key, $value);
+			}
+
+			return $rule->getHandler()->handle($request);
+    	}
+
+    	if ($result->getType() === $result::METHOD_NOT_MATCHED) {
+    		return $handler->handle($request)
+    			->withStatus(405, 'Method Not Allowed')
+    			->withHeader('Allow', implode(',', $result->getAllowedMethods()));
+    	}
+
+    	return $handler->handle($request)->withStatus(404, 'Not Found');
+    }
+
+    /**
+     * [dispatch description]
+     *
+     * @param MatchResult $result [description]
+     * @throws RequestTargetNotMatchedException [<description>]
+     * @throws MethodNotMatchedException [<description>]
+     * @return Response [description]
+     */
+    public static function dispatch(MatchResult $result): Response
+    {
+    	$request = $result->getRequest();
+
+    	if ($result->getType() === $result::REQUEST_TARGET_NOT_MATCHED) {
+    		throw new RequestTargetNotMatchedException($request->getRequestTarget());
+    	}
+
+    	if ($result->getType() === $result::METHOD_NOT_MATCHED) {
+    		throw new MethodNotMatchedException($request->getMethod(), $result->getAllowedMethods());
+    	}
+
+    	$rule = $result->getMatchedRule();
+
+		foreach ($rule->getPattern()->getPlaceholders() as $key => $value) {
+			$request = $request->withAttribute($key, $value);
+		}
+
+		return $rule->getHandler()->handle($request);
+    }
+}
---
+++ tests/DispatcherTest.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Route;
+
+use Meraki\TestSuite;
+use Meraki\Route\Dispatcher;
+use Meraki\Route\Mapper;
+use Meraki\Route\Matcher;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ServerRequestInterface as ServerRequest;
+use Psr\Http\Message\ResponseInterface as Response;
+use Zend\Diactoros\Response\EmptyResponse;
+use Zend\Diactoros\ServerRequestFactory;
+
+final class DispatcherTest extends TestSuite
+{
+	private $handler;
+
+	public function setUp(): void
+	{
+		$this->handler = new class() implements RequestHandler {
+			public $requestPassedToHandle;
+			public function handle(ServerRequest $request): Response {
+				$this->requestPassedToHandle = $request;
+				return new EmptyResponse();
+			}
+		};
+
+		$this->mapper = new Mapper();
+		$this->mapper->get('/', $this->handler);
+		$this->mapper->get('/say-hello/:person', $this->handler);
+
+		$this->matcher = new Matcher($this->mapper);
+
+		$this->serverRequest = (new ServerRequestFactory())->createServerRequest('GET', '/');
+	}
+
+    /**
+     * @test
+     */
+    public function it_exists(): void
+    {
+        $itExists = class_exists(Dispatcher::class);
+
+        $this->assertTrue($itExists);
+    }
+
+    /**
+     * @test
+     */
+    public function dispatching_as_middleware_invokes_handler_if_match_result_is_successful(): void
+    {
+    	$dispatcher = new Dispatcher($this->matcher);
+
+    	$response = $dispatcher->process($this->serverRequest, $this->createMock(RequestHandler::class));
+
+    	$this->assertInstanceOf(Response::class, $response);
+    }
+
+    /**
+     * @test
+     */
+    public function dispatching_as_middleware_sets_placeholders_as_request_attributes_if_match_result_is_successful(): void
+    {
+    	$dispatcher = new Dispatcher($this->matcher);
+    	$serverRequest = $this->serverRequest->withRequestTarget('/say-hello/Nathan');
+
+    	$response = $dispatcher->process($serverRequest, $this->createMock(RequestHandler::class));
+
+    	$this->assertEquals('Nathan', $this->handler->requestPassedToHandle->getAttribute('person'));
+    }
+
+    /**
+     * @test
+     */
+    public function dispatching_as_middleware_modifies_response_with_405_status_if_method_wasnt_matched(): void
+    {
+    	$dispatcher = new Dispatcher($this->matcher);
+    	$serverRequest = $this->serverRequest->withMethod('OPTIONS');
+
+    	$response = $dispatcher->process($serverRequest, $this->handler);
+
+    	$this->assertEquals(405, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function dispatching_as_middleware_returns_405_response_with_allow_header_set(): void
+    {
+    	$dispatcher = new Dispatcher($this->matcher);
+    	$serverRequest = $this->serverRequest->withMethod('OPTIONS');
+
+    	$response = $dispatcher->process($serverRequest, $this->handler);
+
+    	$this->assertEquals('GET', $response->getHeaderLine('allow'));
+    }
+
+    /**
+     * @test
+     */
+    public function dispatching_as_middleware_modifies_response_with_404_status_if_request_target_wasnt_matched(): void
+    {
+    	$dispatcher = new Dispatcher($this->matcher);
+    	$serverRequest = $this->serverRequest->withRequestTarget('/say-goodbye');
+
+    	$response = $dispatcher->process($serverRequest, $this->handler);
+
+    	$this->assertEquals(404, $response->getStatusCode());
+    }
+}
