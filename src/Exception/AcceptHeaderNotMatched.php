59e69e88dcf9edef4cee38eec8ae10494ce8dbad
Nathan Bishop <nbish11@hotmail.com>
2019-08-16 11:55:05 +1000

    Provide an exception for when an accept header is not matched

---
+++ src/Exception/AcceptHeaderNotMatched.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Route\Exception;
+
+use RuntimeException;
+use Meraki\Route\Exception as RouteException;
+use LogicException;
+use RecursiveIteratorIterator;
+use RecursiveArrayIterator;
+
+/**
+ * Exception used when the 'accept' header could not be matched.
+ *
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2019 Nathan Bishop
+ * @license The MIT license.
+ */
+final class AcceptHeaderNotMatched extends RuntimeException implements RouteException
+{
+	/**
+	 *
+	 */
+	public const EQUIVALENT_STATUS_CODE = 406;
+
+	/**
+	 * @var string [$acceptHeader description]
+	 */
+	private $acceptHeader;
+
+	/**
+	 * @var string[] [$allowedMediaTypes description]
+	 */
+	private $allowedMediaTypes;
+
+	/**
+	 * [__construct description]
+	 *
+	 * @param string $acceptHeader [description]
+	 * @param string[] $allowedMediaTypes [description]
+	 */
+	public function __construct(string $acceptHeader, array $allowedMediaTypes = [])
+	{
+		$this->acceptHeader = $acceptHeader;
+		$this->allowedMediaTypes = $allowedMediaTypes;
+
+		parent::__construct($this->generateMessage(), self::EQUIVALENT_STATUS_CODE);
+	}
+
+	/**
+	 * [getAcceptHeader description]
+	 *
+	 * @return string [description]
+	 */
+	public function getAcceptHeader(): string
+	{
+		return $this->acceptHeader;
+	}
+
+	/**
+	 * [getAllowedMediaTypes description]
+	 *
+	 * @return string[] [description]
+	 */
+	public function getAllowedMediaTypes(): array
+	{
+		return $this->allowedMediaTypes;
+	}
+
+	/**
+	 * [generateMessage description]
+	 *
+	 * @return string [description]
+	 */
+	private function generateMessage(): string
+	{
+		$message = 'A representation could not be generated for the requested media-types.';
+
+		if (!empty($this->allowedMediaTypes)) {
+			$allowedMediaTypes = new RecursiveIteratorIterator(new RecursiveArrayIterator($this->allowedMediaTypes));
+			$message .= ' Try one of the following: ';
+
+			foreach ($allowedMediaTypes as $mediaType) {
+				$message .= $mediaType . ', ';
+			}
+
+			$message = rtrim($message, ', ');
+		}
+
+		return $message;
+	}
+}
---
+++ tests/Exception/AcceptHeaderNotMatchedTest.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Route\Exception;
+
+use Meraki\Route\Exception\AcceptHeaderNotMatched;
+use Meraki\Route\Exception as RouteException;
+use Meraki\TestSuite;
+use RuntimeException;
+
+final class AcceptHeaderNotMatchedTest extends TestSuite
+{
+	/**
+	 * @test
+	 */
+	public function it_implements_the_route_components_base_exception(): void
+	{
+		$acceptHeaderNotMatched = new AcceptHeaderNotMatched('text/xml');
+
+		$this->assertInstanceOf(RouteException::class, $acceptHeaderNotMatched);
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_is_a_runtime_exception(): void
+	{
+		$acceptHeaderNotMatched = new AcceptHeaderNotMatched('text/xml');
+
+		$this->assertInstanceOf(RuntimeException::class, $acceptHeaderNotMatched);
+	}
+
+	/**
+	 * @test
+	 */
+	public function accept_header_that_failed_match_can_be_retrieved(): void
+	{
+		$expectedAcceptHeader = 'text/xml';
+		$acceptHeaderNotMatched = new AcceptHeaderNotMatched($expectedAcceptHeader);
+
+		$actualAcceptHeader = $acceptHeaderNotMatched->getAcceptHeader();
+
+		$this->assertEquals($expectedAcceptHeader, $actualAcceptHeader);
+	}
+
+	/**
+	 * @test
+	 */
+	public function can_get_allowed_media_types(): void
+	{
+		$expectedAllowedMediaTypes = ['application/json', 'text/html'];
+		$acceptHeaderNotMatched = new AcceptHeaderNotMatched('text/xml', $expectedAllowedMediaTypes);
+
+		$actualAllowedMediaTypes = $acceptHeaderNotMatched->getAllowedMediaTypes();
+
+		$this->assertEquals($expectedAllowedMediaTypes, $actualAllowedMediaTypes);
+	}
+
+	/**
+	 * @test
+	 */
+	public function sets_correct_message_when_no_allowed_media_types_were_provided(): void
+	{
+		$expectedMessage = 'A representation could not be generated for the requested media-types.';
+		$acceptHeaderNotMatched = new AcceptHeaderNotMatched('text/xml');
+
+		$actualMessage = $acceptHeaderNotMatched->getMessage();
+
+		$this->assertEquals($expectedMessage, $actualMessage);
+	}
+
+	/**
+	 * @test
+	 */
+	public function sets_correct_message_when_allowed_media_types_are_provided(): void
+	{
+		$expectedMessage = 'A representation could not be generated for the requested media-types. Try one of the following: text/html, application/json';
+		$acceptHeaderNotMatched = new AcceptHeaderNotMatched('text/xml', ['text/html', 'application/json']);
+
+		$actualMessage = $acceptHeaderNotMatched->getMessage();
+
+		$this->assertEquals($expectedMessage, $actualMessage);
+	}
+
+	/**
+	 * @test
+	 */
+	public function code_is_set_to_the_equivalent_http_status_code(): void
+	{
+		$expectedCode = 406;
+		$acceptHeaderNotMatched = new AcceptHeaderNotMatched('text/xml');
+
+		$actualCode = $acceptHeaderNotMatched->getCode();
+
+		$this->assertEquals($expectedCode, $actualCode);
+	}
+}
