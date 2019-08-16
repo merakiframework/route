c751f8e12075c68cc006c38d0d9e57e26984cee9
Nathan Bishop <nbish11@hotmail.com>
2019-08-16 12:23:24 +1000

    Add missing test cases for when an accept header is not matched

--- tests/MatchResultTest.php
+++ tests/MatchResultTest.php
@@ -19,6 +19,7 @@ final class MatchResultTest extends TestSuite
 		$this->handler = $this->createMock(RequestHandler::class);
 		$this->rule = Rule::create($this->method, $this->requestTarget, $this->handler);
 		$this->allowedMethods = ['GET', 'POST'];
+		$this->supportedMediaTypes = ['application/json', 'text/html'];
 	}
 
     /**
@@ -241,6 +242,78 @@ final class MatchResultTest extends TestSuite
     public function a_method_not_matched_result_is_considered_a_failure(): void
     {
     	$result = MatchResult::methodNotMatched($this->request, $this->allowedMethods);
+
+    	$actual = $result->isFailure();
+
+    	$this->assertTrue($actual);
+    }
+
+	/**
+     * @test
+     */
+    public function an_accept_header_not_matched_result_has_the_correct_type(): void
+    {
+    	$result = MatchResult::acceptHeaderNotMatched($this->request, $this->supportedMediaTypes);
+
+    	$actual = $result->getType();
+
+    	$this->assertEquals(MatchResult::ACCEPT_HEADER_NOT_MATCHED, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function an_accept_header_not_matched_result_provides_the_request_object(): void
+    {
+    	$result = MatchResult::acceptHeaderNotMatched($this->request, $this->supportedMediaTypes);
+
+    	$actual = $result->getRequest();
+
+    	$this->assertSame($this->request, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function an_accept_header_not_matched_result_does_not_have_a_matched_rule_set(): void
+    {
+    	$result = MatchResult::acceptHeaderNotMatched($this->request, $this->supportedMediaTypes);
+
+    	$actual = $result->getMatchedRule();
+
+    	$this->assertNull($actual);
+    }
+
+    /**
+     * @test
+     */
+    public function an_accept_header_not_matched_result_provides_the_media_types_that_are_supported(): void
+    {
+    	$result = MatchResult::acceptHeaderNotMatched($this->request, $this->supportedMediaTypes);
+
+    	$actual = $result->getAllowedMediaTypes();
+
+    	$this->assertEquals($this->supportedMediaTypes, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function an_accept_header_not_matched_result_is_not_considered_successful(): void
+    {
+    	$result = MatchResult::acceptHeaderNotMatched($this->request, $this->supportedMediaTypes);
+
+    	$actual = $result->isSuccessful();
+
+    	$this->assertFalse($actual);
+    }
+
+    /**
+     * @test
+     */
+    public function an_accept_header_not_matched_result_is_considered_a_failure(): void
+    {
+    	$result = MatchResult::acceptHeaderNotMatched($this->request, $this->supportedMediaTypes);
 
     	$actual = $result->isFailure();
 
