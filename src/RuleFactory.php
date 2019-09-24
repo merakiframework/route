0aff4cf6eeca1c3041e6183dd2d644ad04fdacd6
Nathan Bishop <nbish11@hotmail.com>
2019-09-24 15:12:58 +1000

    Change signature to allow only PSR request-handlers

--- src/RuleFactory.php
+++ src/RuleFactory.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Meraki\Route;
 
+use Meraki\Route\Rule;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
 interface RuleFactory
 {
-	public function make(string $method, string $pattern, callable $handler): Rule;
+	public function make(string $method, string $pattern, RequestHandler $handler): Rule;
 }
